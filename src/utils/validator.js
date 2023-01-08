4341bbb87f0606dad848dbf5aa228141caa71484
leviheart <15720605398@163.com>
2023-01-08 23:14:32 +0800

    wcx格式化

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -3,38 +3,38 @@ const resPass = /^[a-zA-Z0-9]{6,18}$/;
 const regEmail = /1/;
 /**
  * 校验手机号
- * @param {*} rule 
- * @param {*} value 
- * @param {*} callback 
+ * @param {*} rule
+ * @param {*} value
+ * @param {*} callback
  */
 export function validatorPhone(rule, value, callback) {
-    if (regPhone.test(value)) {
-        callback()
-    } else {
-        callback(new Error("请输入11位数字的手机号"))
-    }
+  if (regPhone.test(value)) {
+    callback();
+  } else {
+    callback(new Error("请输入11位数字的手机号"));
+  }
 }
 
 /**
  * 校验邮箱
- * @param {*} rule 
- * @param {*} value 
- * @param {*} callback 
+ * @param {*} rule
+ * @param {*} value
+ * @param {*} callback
  */
 export function validatorEmail(rule, value, callback) {
-    if (regEmail.test(value)) {
-        callback()
-    } else {
-        callback(new Error("请输入11位数字的手机号"))
-    }
+  if (regEmail.test(value)) {
+    callback();
+  } else {
+    callback(new Error("请输入11位数字的手机号"));
+  }
 }
 
 /**
  * 校验密码
- * @param {*} rule 
- * @param {*} value 
- * @param {*} callback 
+ * @param {*} rule
+ * @param {*} value
+ * @param {*} callback
  */
 export function validatorPass(value) {
-    return resPass.test(value)
+  return resPass.test(value);
 }
