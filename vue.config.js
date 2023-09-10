0b40bf73dd9196d817cb60eedc267af227deabcd
莱维 <15720605398@163.com>
2023-09-10 20:50:03 +0800

    ./与/

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 module.exports = {
   // 基本路径
-  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "./",
+  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
   // 输出文件目录
   outputDir: process.env.NODE_ENV === "production" ? "dist" : "devdist",
   // eslint-loader 是否在保存的时候检查
